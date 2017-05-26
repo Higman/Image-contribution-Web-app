dde7569122fe67a3db4c5643b728b9ae14cdb44d
Higman <s15t201@stu.kagawa-u.ac.jp>
2017-05-26 18:06:30 +0000

    rename app

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Bokete2
+module Bokete
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
