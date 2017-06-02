47be83725c47646d26923441e7091bf30b1a21f7
Higman <s15t201@stu.kagawa-u.ac.jp>
2017-06-02 07:44:00 +0000

    modify comment_spec.rb

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe Comment, type: :model do
 
       it "require" do
         expect(no_body_comment).to_not be_valid
-        expect(no_body_comment.errors[:body].size).to be
+        expect(no_body_comment.errors[:body].size).to be 1
       end
     end
   end
